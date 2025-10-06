8a0995c89ad24a14cea5c2dc3c418f028ae7ecdd
Uko Chibuike Malachi <113272653+malachi43@users.noreply.github.com>
2025-10-06 10:15:39 +0100

    created method 'floorOfANumber' and refactored ceilingOfANumber to ceilOfANumber in BinarySearch.java class

--- src/main/java/com/malachi/DSA/BinarySearch.java
+++ src/main/java/com/malachi/DSA/BinarySearch.java
@@ -4,14 +4,6 @@ import java.util.Arrays;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7,8,9};
-        int target = 8;
-        int ans = binarySearch(arr, target);
-        System.out.println("index of " + target + " is: " + ans);
-        int[] arrNum = {2,3,5,9,14,16,18};
-        int numberToCeil = 15;
-        int result = ceilingOfANumber(arrNum, numberToCeil);
-        System.out.println("The ceiling of " + numberToCeil + " from this array, " + Arrays.toString(arrNum) + " is: " + result);
     }
 
     public static int binarySearch(int[] arr, int target){
@@ -48,7 +40,7 @@ public class BinarySearch {
         return -1;
     }
 
-    public static int ceilingOfANumber(int[] arr, int target){
+    public static int ceilOfANumber(int[] arr, int target){
         int start = 0;
         int end = arr.length - 1;
 
@@ -64,6 +56,25 @@ public class BinarySearch {
         }
         //at this point, start is the index of the smallest value greater than target i.e the ceil of the number.
         return start;
+
+    }
+
+    public static int floorOfANumber(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(arr[mid] == target) return arr[mid];
+            else if(target > arr[mid]){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+
+        }
+        //at this point, end is the index of the greatest value smaller than target i.e the floor of the number.
+        return end;
 
     }
 }
